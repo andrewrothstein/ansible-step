368682cd535d5666c46660b5da7afe32c3d26d4c
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-14 23:56:45 -0400

    step:0.18.2

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -9,12 +9,13 @@ dl()
     local os=$2
     local arch=$3
     local platform="${os}_${arch}"
-    local file=step_${os}_${ver}_${arch}.tar.gz
+    local archive_type=${4:-tar.gz}
+    local file=step_${os}_${ver}_${arch}.${archive_type}
     local url=$MIRROR/v$ver/$file
     local lfile=$DIR/$file
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -24,10 +25,12 @@ dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
     dl $ver darwin amd64
+    dl $ver darwin arm64
     dl $ver linux amd64
     dl $ver linux arm64
     dl $ver linux armv7
-    dl $ver windows amd64
+    dl $ver windows amd64 zip
+    dl $ver windows arm64 zip
 }
 
-dl_ver ${1:-0.17.7}
+dl_ver ${1:-0.18.2}
